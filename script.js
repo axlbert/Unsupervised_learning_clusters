15524cf61df9d68590072292768849aacd89959c
axlbert <aclanger131@gmail.com>
2016-03-20 13:48:09 +0100

    complete redesign

--- script.js
+++ script.js
@@ -9,8 +9,10 @@
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
           $(".navbar-default").css('background-color', '#ffffff').css('box-shadow', '1px 1px 5px #888888');
+
        } else {
           $('.navbar-default').css('background-color', 'transparent').css('box-shadow', 'none');
+
        }
    });
     }
